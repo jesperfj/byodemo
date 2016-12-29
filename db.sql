4124ced4c816633d28c2811cab1a85a6c8999d9c
Jesper Joergensen <jesper@heroku.com>
2016-12-28 23:35:59 -0800

    This creates a new db schema from scratch matching current db.

--- db.sql
+++ db.sql
@@ -1,2 +1,15 @@
-create table accounts (owner_uuid uuid, aws_access_key_id character varying, aws_secret_access_key character varying);
-create table addon_resources (owner_uuid uuid, provider_resource_id character varying, heroku_resource_id character varying);
+CREATE TABLE accounts (
+    owner_uuid uuid,
+    aws_access_key_id character varying,
+    aws_secret_access_key_token bytea
+);
+
+
+CREATE TABLE addon_resources (
+    owner_uuid uuid,
+    provider_resource_id character varying,
+    heroku_resource_id character varying,
+    deleted_at timestamp without time zone,
+    mark_for_deletion boolean DEFAULT false,
+    aws_access_key_id character varying
+);
